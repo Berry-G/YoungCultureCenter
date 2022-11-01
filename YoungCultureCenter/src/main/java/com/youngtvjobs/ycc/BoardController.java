e676eb9d0ce47d5e132bbce6ddd8f4b52cd99e0d
alwaysFinn <dhksthwlgh@naver.com>
2022-11-01 19:17:33 +0900

    board controller

--- YoungCultureCenter/src/main/java/com/youngtvjobs/ycc/BoardController.java
+++ YoungCultureCenter/src/main/java/com/youngtvjobs/ycc/BoardController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 		public String noticeBoard() {
 			return "board/noticeBoard";
 		}
-		
+		 
 		@RequestMapping("/eventBoard")
 		public String eventBoard() {
 			return "board/eventBoard";
