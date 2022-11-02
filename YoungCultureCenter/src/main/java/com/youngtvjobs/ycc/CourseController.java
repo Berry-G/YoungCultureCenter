42ff3da4b8ca8826e108b7c4904ddc38e904510a
cyh6327 <cyh6327@gmail.com>
2022-11-02 13:38:38 +0900

    CourseController.java , courseSearch.css , search.jsp upload

---
+++ YoungCultureCenter/src/main/java/com/youngtvjobs/ycc/CourseController.java
@@ -0,0 +1,13 @@
+package com.youngtvjobs.ycc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CourseController {
+
+	@RequestMapping("/course/search")
+	public String courseSearch() {
+		return "/course/search";
+	}
+}
