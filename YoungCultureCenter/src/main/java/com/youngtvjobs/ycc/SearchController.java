0d36d5432e57c2456a2a01c005f1fa1f53f240b6
alwaysFinn <dhksthwlgh@naver.com>
2022-11-02 23:44:10 +0900

    SearchController commit

---
+++ YoungCultureCenter/src/main/java/com/youngtvjobs/ycc/SearchController.java
@@ -0,0 +1,27 @@
+package com.youngtvjobs.ycc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+//검색창 컨트롤러
+@Controller
+public class SearchController {
+
+		@RequestMapping("/search")
+		public String searchPage()
+		{
+			return "search/search";
+		}
+
+		@RequestMapping("/search/all")
+		public String searchAll()
+		{
+			return "search/all";
+		}
+
+		
+
+}
+	
+	
