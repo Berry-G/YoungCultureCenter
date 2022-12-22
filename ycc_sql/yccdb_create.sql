22236d349073059bac8cb0972e85860b625c24d0
지호 <106485220+alwaysFinn@users.noreply.github.com>
2022-12-22 20:21:20 +0900

    Update yccdb_create.sql

--- ycc_sql/yccdb_create.sql
+++ ycc_sql/yccdb_create.sql
@@ -137,9 +137,7 @@ CREATE TABLE inq_board (
     inq_id    serial NOT null,
     inq_title varchar(100) not null,
     inq_content text not null,
-    inq_date date not null,
-    inq_YN    boolean NOT null
-    
+    inq_date date not null,    
 );
 ALTER TABLE inq_board ADD CONSTRAINT inq_board_PK PRIMARY KEY ( inq_id );
 alter table inq_board add column inq_ans text;
