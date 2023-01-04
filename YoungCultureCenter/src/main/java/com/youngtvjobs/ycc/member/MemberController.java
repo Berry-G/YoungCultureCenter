718d89571296fd11df86363814364747e6873416
asel0joo <soojeon@nate.com>
2023-01-04 17:27:05 +0900

    delete import

--- YoungCultureCenter/src/main/java/com/youngtvjobs/ycc/member/MemberController.java
+++ YoungCultureCenter/src/main/java/com/youngtvjobs/ycc/member/MemberController.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
