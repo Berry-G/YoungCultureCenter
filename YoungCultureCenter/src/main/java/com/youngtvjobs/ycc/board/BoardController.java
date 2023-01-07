a91e0877bf20d53190e8a84322f5c154ac133f3b
asel0joo <soojeon@nate.com>
2023-01-07 14:07:41 +0900

    Clean up code

--- YoungCultureCenter/src/main/java/com/youngtvjobs/ycc/board/BoardController.java
+++ YoungCultureCenter/src/main/java/com/youngtvjobs/ycc/board/BoardController.java
@@ -74,8 +74,7 @@ public class BoardController
 	
 	//게시글 상세 보기 
 	@GetMapping("/post")
-	public String postSelect(SearchItem sc,
-		Integer article_id, Model m) {
+	public String postSelect(Integer article_id, Model m) {
 	
 		try {
 			BoardDto boardDto = boardService.postSelect(article_id);
@@ -97,20 +96,17 @@ public class BoardController
 	
 	//게시글 작성 접속 
 	@GetMapping("/write")
-	public String write(BoardDto boardDto, Model model ,HttpServletRequest request) throws Exception {
+	public String write() {
 		
 			return "board/write";
 	}
 	
-
 	//게시글 작성 
 	@PostMapping("/write")
-	public String writePage(BoardDto boardDto, RedirectAttributes rttr, HttpServletRequest request,
-			Model model, HttpSession session, Principal principal) throws Exception {		
+	public String writePage(BoardDto boardDto, Principal principal) throws Exception {		
 
 			boardDto.setUser_id(principal.getName());
-			System.out.println("가나다라" + boardDto);
-			System.out.println();
+
 			try {		
 				boardService.writeInsert(boardDto);
 
@@ -131,38 +127,36 @@ public class BoardController
 			
 		return "redirect:/board/notice";
 	}
+	
 	//게시글 삭제
-	   @PostMapping("/remove")
-	   public String remove(BoardDto boardDto ,Integer article_id, Integer page, Integer pageSize, HttpServletRequest request,
-	                  RedirectAttributes rattr, HttpSession session) {
-	      
-	      try {
-	         BoardDto tmpboard=boardService.getArticleEdit(article_id);
-	         System.out.println( tmpboard);
-	            if(boardService.remove(article_id)== 1) {
-	               //boardDto에서 받은 board-type이 "N"이면 공지사항게시판에 insert
-	               if(tmpboard.getArticle_Board_type().equals("공지사항") ) {
-	                  //insert 후 공지사항 게시판으로 보여줌
-	                  return "redirect:/board/notice";               
-	               }
-	               //boardDto에서 받은 board-type이 "E"이면 이벤트/행사 게시판에 insert
-	               else if(tmpboard.getArticle_Board_type().equals("이벤트") ) {
-	                  //insert 후 이벤트 게시판으로 보여줌 
-	                  return "redirect:/board/event";
-	               }
-	            }
-	         
-	         
-	      } catch (Exception e) {
-	         e.printStackTrace();
-	      }
-	      
-	      return "redirect:/board/notice";
-	   }
+   @PostMapping("/remove")
+   public String remove(Integer article_id) {
+      
+      try {
+         BoardDto tmpboard=boardService.getArticleEdit(article_id);
+         System.out.println( tmpboard);
+            if(boardService.remove(article_id)== 1) {
+               //boardDto에서 받은 board-type이 "N"이면 공지사항게시판에 insert
+               if(tmpboard.getArticle_Board_type().equals("공지사항") ) {
+                  //insert 후 공지사항 게시판으로 보여줌
+                  return "redirect:/board/notice";               
+               }
+               //boardDto에서 받은 board-type이 "E"이면 이벤트/행사 게시판에 insert
+               else if(tmpboard.getArticle_Board_type().equals("이벤트") ) {
+                  //insert 후 이벤트 게시판으로 보여줌 
+                  return "redirect:/board/event";
+               }
+            }     
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+      
+      return "redirect:/board/notice";
+   }
 	   
 	//게시글 수정페이지로 이동
 	@GetMapping("/edit")
-	public String getArticleEdit(Integer article_id, Model m, HttpServletRequest request) throws Exception {
+	public String getArticleEdit(Integer article_id, Model m) {
 		//boardMapper.xml에 select값을 가져오는 로직
 
 		try {
@@ -171,13 +165,14 @@ public class BoardController
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 	 return "board/edit";
+	 
 	}
 		
 	//게시글 수정(등록)
 	@PostMapping("/edit1")
-	public String modify(BoardDto boardDto, Integer page, Integer pageSize, 
-						RedirectAttributes rattr, Model m,Principal principal ,HttpSession session) {
+	public String modify(BoardDto boardDto, Principal principal) {
 		
 		boardDto.setUser_id(principal.getName());
 		//등록버튼 누를 시 수정됨
@@ -202,6 +197,7 @@ public class BoardController
 	//자주 묻는 질문(FAQ)
 	@GetMapping("/faq")
 	public String faq() {
+		
 		return "board/faq";
 	}
 
